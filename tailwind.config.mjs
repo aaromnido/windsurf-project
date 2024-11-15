5d8d34642e2864c111c09d6b7a8294e44ef3dcb3
Fernando <fernando@inspiration-q.com>
2024-11-15 18:08:02 +0100

    Improve the index layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Plus Jakarta Sans', 'sans-serif'],
+      },
       colors: {
         primary: {
           DEFAULT: 'var(--primary)',
